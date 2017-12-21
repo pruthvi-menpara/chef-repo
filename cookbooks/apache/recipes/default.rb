293df18cb90d9af6ade1aac58537d60de023f2b3
pruthvi-menpara <pruthvi.menpara@gmail.com>
2017-12-21 03:57:27 +0000

    Only_if example

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -13,10 +13,17 @@ end
 
 # Create File
 
-cookbook_file "/var/www/html/index.html" do
-source node["apache"]["indexfile"]
-mode "0644"
+#cookbook_file "/var/www/html/index.html" do
+#source node["apache"]["indexfile"]
+#mode "0644"
+#end
+
+#Rename conf.d file
+execute "mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.bak" do
+	only_if do File.exist?("/etc/httpd/conf.d/welcome.conf") end
+	notifies :restart, 'service[httpd]'
 end
+
 
 # Service Start and Enable
 
